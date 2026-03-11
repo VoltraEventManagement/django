bb6739682ccf46f33e9e57ceb3a4cdb92d33b339
seba ibraheem <sebaalmahdy@gmail.com>
2026-03-11 05:58:28 +0200

    final touches

--- core/settings.py
+++ core/settings.py
@@ -35,11 +35,17 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
+CORS_ALLOW_ALL_ORIGINS = True  
+# Allow all methods (GET, POST, PUT, DELETE, etc.)
+CORS_ALLOW_METHODS = ["*"]
 
+# Allow all headers
+CORS_ALLOW_HEADERS = ["*"]
+CORS_ALLOW_CREDENTIALS = True
 # Application definition
 
 INSTALLED_APPS = [
